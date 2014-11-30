536121bcbec85914582ea5f3f3af97355ec64ba4
Anvar Azizov <anvardejim@gmail.com>
2014-12-01 00:14:48 +0200

    Possessives added.

--- Grammar.playground/section-1.swift
+++ Grammar.playground/section-1.swift
@@ -47,4 +47,74 @@ extension String {
 			return self + "ga"
 		}
 	}
+	
+	// MARK: Possessive
+	
+	func my() -> String {
+		
+		switch last(self) as Character! {
+		case "a", "e", "i", "o", "u":
+			return self + "m"
+		case "k":
+			return dropLast(self) + "gim"
+		case "q":
+			return dropLast(self) + "g'im"
+		default:
+			return self + "im"
+		}
+	}
+	
+	func your() -> String {
+		
+		switch last(self) as Character! {
+		case "a", "e", "i", "o", "u":
+			return self + "ng"
+		case "k":
+			return dropLast(self) + "ging"
+		case "q":
+			return dropLast(self) + "g'ing"
+		default:
+			return self + "ing"
+		}
+	}
+	
+	func yourPlural() -> String {
+		return self.your() + "iz"
+	}
+	
+	func his() -> String {
+		
+		switch last(self) as Character! {
+		case "a", "e", "i", "o", "u":
+			return self + "si"
+		case "k", "q":
+			return dropLast(self) + "i"
+		default:
+			return self + "i"
+		}
+	}
+	
+	func their() -> String {
+		
+		switch last(self) as Character! {
+		case "k", "q":
+			return dropLast(self) + "lari"
+		default:
+			return self + "lari"
+		}
+	}
+	
+	func our() -> String {
+		
+		switch last(self) as Character! {
+		case "a", "e", "i", "o", "u":
+			return self + "miz"
+		case "k":
+			return dropLast(self) + "gimiz"
+		case "q":
+			return dropLast(self) + "g'imiz"
+		default:
+			return self + "imiz"
+		}
+	}
 }
